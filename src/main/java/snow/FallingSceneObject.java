d36be51848b34368b8db8882a526f9d7b180ac23
Mikael Svensson <github@mikaelsvensson.info>
2014-11-18 19:15:04 +0100

    More random snowflake rotation

--- src/main/java/snow/FallingSceneObject.java
+++ src/main/java/snow/FallingSceneObject.java
@@ -4,6 +4,7 @@ import java.awt.*;
 
 public abstract class FallingSceneObject extends SceneObject {
     private static final long MINIMUM_SECONDS_FOR_FALL = 5;
+    private double rotationDirection = Util.random(0.3, 1.0) * (Util.random(0, 1) > 0.5 ? 1 : -1);
 
     public FallingSceneObject(int requestedWidthPixels, int requestedHeightPixels, double z, Rectangle sceneBounds) {
         super(requestedWidthPixels,
@@ -18,7 +19,7 @@ public abstract class FallingSceneObject extends SceneObject {
         double delta = msSinceLastUpdate * changePerMillisecond;
 
         y += delta;
-        double degreesPerMillisecond = speed * (360.0 / MINIMUM_SECONDS_FOR_FALL / 1000);
+        double degreesPerMillisecond = rotationDirection * (360.0 / MINIMUM_SECONDS_FOR_FALL / 1000);
         rotation = (rotation + (degreesPerMillisecond * msSinceLastUpdate)) % 360;
     }
 }
